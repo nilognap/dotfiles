30322a30a53bd93b1c589c1d735baf1300401ae7
nilognap <wanghan4c3@gmail.com>
2025-08-14 16:51:25 +0200

    .

--- nixos/services/keyd.nix
+++ nixos/services/keyd.nix
@@ -1,46 +1,47 @@
 {
   services.keyd = {
-    # enable = true;
+    enable = true;
     keyboards = {
       default = {
         ids = [ "*" ];
-        settings = {
-          main = let
-            # minimum amount of time waited after the last key was struck for mod
-            idle_timeout = "0";
-            # the amount of time the key need to be held down for mod
-            hold_timeout = "300";
-          in {
-            capslock = "backspace";
-            leftshift = "b"; # angle mod
-
-            a = "lettermod(meta, a, ${idle_timeout}, ${hold_timeout})";
-            s = "lettermod(alt, s, ${idle_timeout}, ${hold_timeout})";
-            d = "lettermod(control, d, ${idle_timeout}, ${hold_timeout})";
-            f = "lettermod(shift, f, ${idle_timeout}, ${hold_timeout})";
-
-            j = "lettermod(shift, j, ${idle_timeout}, ${hold_timeout})";
-            k = "lettermod(control, k, ${idle_timeout}, ${hold_timeout})";
-            l = "lettermod(alt, l, ${idle_timeout}, ${hold_timeout})";
-            ";" = "lettermod(meta, ;, ${idle_timeout}, ${hold_timeout})";
-
-            space = "lettermod(extend, space, ${idle_timeout}, ${hold_timeout})";
-
-            delete = "toggle(gaming)";
+        settings = let
+          # minimum amount of time waited after the last key was struck for mod
+          idle_timeout = "0";
+          # the amount of time the key need to be held down for mod
+          hold_timeout = "300";
+        in {
+          main = { # external keyboard
+            delete = "toggle(laptop)";
           };
-          extend = {
-            q = "{"; w = 7; e = 8; r = 9; t = "}";
-            a = "("; s = 4; d = 5; f = 6; g = ")";
-            leftshift = "["; z = 1; x = 2; c = 3; v = "]";
-            leftmeta = 0; leftalt = 0;
+          laptop = {
+            capslock = "backspace";
 
+            space = "lettermod(nav, space, ${idle_timeout}, ${hold_timeout})";
+            # rightalt = "overload(oneshot(shift), layer(sym))";
+            rightalt = "oneshot(shift)";
+            "\\" = "rightalt"; # compose key
+
+            q = "v"; w = "m"; e = "l"; r = "c"; t = "p";
+            a = "lettermod(meta, s, ${idle_timeout}, ${hold_timeout})";
+            s = "lettermod(alt, t, ${idle_timeout}, ${hold_timeout})";
+            d = "lettermod(shift, r, ${idle_timeout}, ${hold_timeout})";
+            f = "lettermod(control, d, ${idle_timeout}, ${hold_timeout})";
+            g = "y";
+            leftshift = "z"; z = "k"; x = "q"; c = "g"; v = "w";
+
+            b = "noop"; y = "noop"; h = "noop"; n = "noop";
+
+            u = "x"; i = "f"; o = "o"; p = "u"; "[" = "j";
+            j = ".";
+            k = "lettermod(control, n, ${idle_timeout}, ${hold_timeout})";
+            l = "lettermod(shift, a, ${idle_timeout}, ${hold_timeout})";
+            ";" = "lettermod(alt, e, ${idle_timeout}, ${hold_timeout})";
+            "'" = "lettermod(meta, i, ${idle_timeout}, ${hold_timeout})";
+            m = "b"; "," = "h"; "." = "'"; "/" = ";"; rightshift = ",";
+          };
+          nav = {
             j = "left"; k = "down"; l = "up"; ";" = "right";
           };
-          # gaming = {
-            # a = "a"; s = "s"; d = "d"; f = "f";
-            # j = "j"; k = "k"; l = "l"; ";" = ";";
-            # space = "space";
-          # };
         };
       };
       externalMapped = {
