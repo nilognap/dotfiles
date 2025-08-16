f1e176065cd1e3549e8a5aafb1d5603545052866
nilognap <wanghan4c3@gmail.com>
2025-08-16 22:59:02 +0200

    .

--- home-manager/cli/programs/fastfetch.nix
+++ home-manager/cli/programs/fastfetch.nix
@@ -3,12 +3,16 @@
     enable = true;
     settings = {
       logo = {
-        padding = { top = 1; left = 2; right = 2; };
+        padding = {
+          # top = 1;
+          left = 2;
+          # right = 1;
+        };
       };
       modules = [
         "title" "separator" "os" "host" "kernel"
         "uptime" "packages" "shell" "display"
-        # "de" "wm" "wmtheme" "theme" "icons"
+        "de" "wm" "wmtheme" "theme" "icons"
         "cpu" "gpu" "memory" "disk" "battry" "poweradapter"
       ];
     };
